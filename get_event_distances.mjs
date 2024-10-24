61730c91b2e020d43d89152e851f1faea6309eca
Mukil Sundaravadivel <67285595+MukilSundaravadivel@users.noreply.github.com>
2024-10-24 13:18:30 -0400

    Add dev files via upload

---
+++ get_event_distances.mjs
@@ -0,0 +1,66 @@
+/**
+ *  run in node with arguments: 
+ *  1) event/district code, 2) location of event
+ *  Example:
+ *  node get_event_data.mjs 2024pch 'dalton convention center dalton georgia'
+ */
+import { readFileSync } from 'fs';
+import { writeFileSync } from 'fs';
+import { unlink } from 'fs';
+
+const geocodingAPIKey = ''; // take out when making code public
+const dataFile = process.argv[2]; // name of file to add distance info to (without extension)
+let eventLocation = process.argv[3] // event location
+
+let combinedData = new Array();
+const getEventDistance = async (data) => {
+    let city;
+    let state;
+    let school;
+    let url;
+    for(var i = 0; i < data.length; i++) {
+        city = data[i].city;
+        state = data[i].state;
+        school = data[i].school_name.replace('/', ' ').replace(/\s/g, '+').split(' &')[0].split('&')[0];
+        eventLocation = eventLocation.replace(' ', '+');
+
+        url = 'https://maps.googleapis.com/maps/api/distancematrix/json?destinations='
+         + eventLocation + '&origins=' + school + '+' + city + '+' + state
+         + '&units=imperial&key=' + geocodingAPIKey;
+        
+        console.log(url);
+
+        let response = await fetch(url);
+        let output = await response.json();
+
+        console.log(output);
+
+        combinedData.push(new Object());
+        combinedData[i] = data[i];
+        combinedData[i].event_distance = output.rows[0].elements[0].distance.text;
+    }
+
+    return combinedData;
+};
+
+(async () => {
+    let teamsDataString = readFileSync('../data/' + dataFile + '.json', 'utf8', function(err, data) {});
+    let teamsData = JSON.parse(teamsDataString);
+
+    await getEventDistance(teamsData); // adds event distance and stores it in combinedData var
+    console.log(combinedData); 
+
+    writeFileSync(
+        '../data/' + dataFile + '.js',
+        "var data = " + JSON.stringify(combinedData, null, 2) + ";"
+    );
+
+    unlink('../data/' + dataFile + '.json', (err) => {
+        if (err) {
+          console.error(`Error removing file: ${err}`);
+          return;
+        }
+      
+        console.log(`json file has been successfully removed.`);
+      });
+})();
---
+++ get_team_data.mjs
@@ -0,0 +1,75 @@
+/**
+ *  run in node with arguments: 
+ *  1) event/district code, 2) true for event/false for district
+ *  Example:
+ *  node get_team_data.mjs 2024pch false
+ */
+import { writeFileSync } from 'fs';
+
+let event = process.argv[2];
+let district = process.argv[2];
+let isEvent = process.argv[3]; 
+
+let url;
+let name;
+
+if (isEvent) {
+    url = 'https://www.thebluealliance.com/api/v3/event/' + event + '/teams';
+    name = event;
+} else {
+    url = 'https://www.thebluealliance.com/api/v3/district/' + district + '/teams';
+    name = district;
+}
+const options = {
+    headers: {
+        'X-TBA-Auth-Key': ''
+    }
+};
+
+let teams = new Array();
+const getTeams = async (url) => {
+    const response = await fetch(url, options);
+    const data = await response.json();
+    for (var i = 0; i < data.length; i++) {
+        teams.push(data[i].team_number);
+    }
+    // teams = [832, 1002, 1261, 1414, 1466, 1648, 1683, 1746, 1771, 1833, 2415, 2974, 3318, 3344, 3635, 4026, 4188, 4189, 4509, 4576, 5074, 5109, 5219, 5293, 5608, 5900, 6023, 6705, 6829, 6905, 6925, 7538, 8080, 8100, 8577, 8736, 8761, 8815, 8866, 9522];
+    return teams;
+};
+
+let teamsData = new Array(); 
+const getTeamsData = async (data) => {
+    for (var i = 0; i < teams.length; i++) {
+        let teamUrl = 'https://www.thebluealliance.com/api/v3/team/frc' + teams[i];
+        let tResponse = await fetch(teamUrl, options);
+        let tData = await tResponse.json();
+        teamsData.push(new Object());
+        teamsData[i] = {
+            'team_number': teams[i],
+            'nickname': tData.nickname,
+            'city': tData.city,
+            'state': tData.state_prov,
+            // 'country': tData.country,
+            'rookie_year': tData.rookie_year,
+            'school_name': tData.school_name
+        }
+        let awardsUrl = 'https://www.thebluealliance.com/api/v3/team/frc' + teams[i] + '/awards';
+        let aResponse = await fetch(awardsUrl, options);
+        let aData = await aResponse.json();
+        teamsData[i].number_of_awards = aData.length;
+    }
+    return data;
+};
+
+(async () => {
+    await getTeams(url);
+    console.log(teams);
+
+    await getTeamsData(teams);
+    console.log(teamsData);
+
+    writeFileSync(
+        '../data/' + name + '.json',
+        JSON.stringify(teamsData, null, 2)
+    );
+})();
